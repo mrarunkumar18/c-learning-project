13325501a17e4aff4313b4aaad08720714466d6c
mrarunkumar18 <mrarunkumar1806@gmail.com>
2025-09-29 03:15:22 +0530

    Organize folders

---
+++ chapter 1/01_Variable.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int a = 10; // Variable declaration and initialization
+    float b = 5.5; // Variable declaration and initialization
+    char c = 'A'; // Variable declaration and initialization
+
+    printf("Integer: %d\n", a);
+    printf("Float: %.2f\n", b);
+    printf("Character: %c\n", c);
+
+    return 0;
+}
