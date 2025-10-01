6b9684f0b8b1f2ab3bf26d567300a3f5524e25ae
mrarunkumar18 <mrarunkumar1806@gmail.com>
2025-10-01 16:25:30 +0530

    Add comments.c && input.c

--- chapter 1/03_data_types.c
+++ chapter 1/03_data_types.c
@@ -1,11 +1,17 @@
-#include<stdio.h>
-int main() {
+#include <stdio.h>
+int main()
+{
     int a = 1; // integer
     // float b = 1.4; // floating point number
     float b;
     b = 1.4; // floating point number
-    char c; 
+    char c;
     c = 'a'; // character
+
+    printf("The value of a is %d\n", a);
+    printf("The value of b is %f\n", b);
+    printf("The value of c is %c\n", c);
+
 
     return 0;
 }
---
+++ chapter 1/05_comments.c
@@ -0,0 +1,14 @@
+/*
+Program : 05_comments.c
+Author: Arun Singh 
+Date : 01/10/2025
+
+*/
+#include<stdio.h>
+int main() {
+    // I am writing this just for fun ,This line doesnt do anything 
+    printf("Hello, World!\n"); 
+    // Return 0 is returning 0 to the operating system
+
+    return 0; // This will return 0 
+}
