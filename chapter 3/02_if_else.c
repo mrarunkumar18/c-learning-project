053d0ec4281e6bf7b3a8760ae539b8e60701e4aa
mrarunkumar18 <mrarunkumar1806@gmail.com>
2025-10-14 19:46:23 +0530

    add arithmetic Operator file

---
+++ chapter 3/02_if_else.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int age = 5;
+
+    if (age > 10)
+    {
+        printf("We are inside if \n");
+        printf("You age is greater than 10\n");
+    }
+    else
+    {
+        printf("your age is less than 10\n");
+    }
+    return 0;
+}
